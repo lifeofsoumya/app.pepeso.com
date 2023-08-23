f255d8ae8ff45eca36833539c22e537c1e7f4479
lifeofsoumya <mondalsoumya2001@gmail.com>
2023-08-23 19:09:32 +0530

    env dev to be on github, meta added, footer glass effect with dynamic routing, meta config with meta

--- components/Meta.js
+++ components/Meta.js
@@ -1,18 +1,28 @@
 import Head from 'next/head'
 import metaConfig from '@/source/configs/metaConfig'
 
-const Meta = ({path, image, title, keywords, description}) =>{
-    const imgPath = image || metaConfig[path] || '';
+const Meta = ({path, image, title, keywords, description, url}) =>{
+    const imgPath = image || `${process.env.NEXT_PUBLIC_BASE_URL}${metaConfig[path]}` || '';
     const ogTitle = title || process.env.NEXT_PUBLIC_SITE_NAME
     const keyWords = keywords || process.env.NEXT_PUBLIC_SITE_NAME
     const ogDesc = description || process.env.NEXT_PUBLIC_SITE_DESC
+    const ogURL = url || process.env.NEXT_PUBLIC_BASE_URL
 
     return(
         <Head>
-            <meta property="og:image" content={imgPath} />
+            <title>{`${title} | app.pepeso.com`}</title>
+            <meta property="og:image" content={imgPath} key="image" />
             <meta property="og:title" content={ogTitle}/>
-            <meta property="og:description" content={ogDesc}/>
+            <meta name="description" content={ogDesc} />
             <meta name="keywords" content = {keyWords} />
+            <meta property="og:title" content={ogTitle} key="title" />
+            <meta property="og:description" content={ogDesc} key="description" />
+            <meta property="og:url" content={ogURL} key="url" />
+            <meta name="twitter:title" content={ogTitle} />
+            <meta name="twitter:description" content={ogDesc} />
+            <meta name="twitter:image" content={imgPath} />
+            <meta name="twitter:card" content="summary_large_image" />
+            <link rel="icon" href="/images/favicon.ico" />
         </Head>
     )
 }
--- source/configs/metaConfig.js
+++ source/configs/metaConfig.js
@@ -3,6 +3,7 @@ const metaConfig = {
     '/apply': '/images/about.jpg',
     '/register': '/images/about.jpg',
     '/blog': '/images/blog.jpg',
+    '/404': '/'
 }
 
 export default metaConfig;
