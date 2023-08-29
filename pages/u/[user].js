fd12a160ac22d3f01ecfe339d3af7ded49222eba
lifeofsoumya <mondalsoumya2001@gmail.com>
2023-08-29 16:59:43 +0530

    dynamic user profile init, made tabs styling and rendered them for now in user page

--- pages/u/[user].js
+++ pages/u/[user].js
@@ -1,8 +1,16 @@
+import TabComponent from '@/source/modules/components/tabs/TabComponent'
 import { useState } from 'react'
 
 const User = () => {
+  const tabData = [
+    {id: 1, title: 'tab1', content: 'ok'},
+    {id: 2, title: 'tab2', content: 'lol'},
+  ]
   return (
+    <>
     <div>User</div>
+    <TabComponent tabData={tabData}/>
+    </>
   )
 }
 
