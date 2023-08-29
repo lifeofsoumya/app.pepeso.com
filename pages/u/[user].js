3f3a5794a069e1ca0a2445f4943c32090cd44f5c
lifeofsoumya <mondalsoumya2001@gmail.com>
2023-08-30 00:02:45 +0530

    router query slug added for single user

--- pages/u/[user].js
+++ pages/u/[user].js
@@ -1,14 +1,17 @@
 import TabComponent from '@/source/modules/components/tabs/TabComponent'
 import { useState } from 'react'
+import { useRouter } from 'next/router'
 
 const User = () => {
+  const router = useRouter()
+  
   const tabData = [
     {id: 1, title: 'tab1', content: 'ok'},
     {id: 2, title: 'tab2', content: 'lol'},
   ]
   return (
     <>
-    <div>User</div>
+    <div>{router.query.user}</div>
     <TabComponent tabData={tabData}/>
     </>
   )
